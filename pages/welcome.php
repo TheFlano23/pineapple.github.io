2005aaa83d3a9265709ef6dd48c2d411d9e46a4b
TheFlano23 <82870101+TheFlano23@users.noreply.github.com>
2023-11-26 01:32:09 +1100

    Initial website upload

---
+++ pages/welcome.php
@@ -0,0 +1,15 @@
+<?php 
+    if(isset($_POST['submit'])){ //if submit button is pressed
+         //code to be executed
+    }else{
+         //code to be executed  
+    }
+?>
+<html>
+<head></head>
+<body>
+     <form method="post" action="">
+        <input type="submit" name="submit">
+     </form>
+</body>
+</html>
